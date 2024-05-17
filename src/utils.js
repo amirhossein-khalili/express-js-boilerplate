81cf5b5c116f5931e389eae0bf24b726c208cadb
amirkhalili <amir1378khalili@gmail.com>
2024-05-17 12:26:39 +0330

    ADD validateSchema

---
+++ src/utils.js
@@ -0,0 +1,19 @@
+const validateSchema = (schema) => {
+  return (req, res, next) => {
+    const { error, value } = schema.validate(req.body, { abortEarly: false });
+    if (error) {
+      const validationErrors = error.details.map((detail) => {
+        return {
+          field: detail.path[0],
+          message: detail.message,
+        };
+      });
+      res.status(400).json({ errors: validationErrors });
+    } else {
+      req.validatedData = value;
+      next();
+    }
+  };
+};
+
+export default validateSchema;
