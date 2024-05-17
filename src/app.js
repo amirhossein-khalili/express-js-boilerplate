72d95129fd2d25d3db5bafc74772b216fb34fb57
amirkhalili <amir1378khalili@gmail.com>
2024-05-17 16:24:06 +0330

    ADD swagger first define to the app.js

--- src/app.js
+++ src/app.js
@@ -1,15 +1,28 @@
 import express from 'express';
 import logger from 'morgan';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerConfig from './config/swagger.js';
 import { connect } from './config/db.js';
 import { restRouter } from './api/index.js';
 
 const app = express();
+connect(); //connect express app to database
 
-connect();
+//middleware and other settings of project app
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Swagger setup
+const swaggerOptions = {
+  swaggerDefinition: swaggerConfig,
+  apis: ['path/to/your/swagger-spec.yaml'], // Path to your Swagger/OpenAPI specification file
+};
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+//api and main routes
 app.use('/api', restRouter);
 app.get('/', (req, res) => {
   res.send({ message: 'welcome to the our application' });
