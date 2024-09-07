85dbe35917cf84ab35b636db450552fea8585357
amir <amir1378khalili@gmail.com>
2024-09-07 21:32:01 +0330

    change user router

--- src/api/resources/user/user.router.js
+++ src/api/resources/user/user.router.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
-import UserController from './user.controller.js';
+import userController from './user.controller.js';
 import UserMiddleware from './user.middleware.js';
-import validateSchema from '../../../utils/validateSchema.utils.js';
+
 import passport from 'passport';
 
 class UserRouter {
@@ -13,18 +13,30 @@ class UserRouter {
   initializeRoutes() {
     this.router
       .route('/')
-      .get(passport.authenticate('jwt', { session: false }), UserController.findAll)
+      .get(
+        passport.authenticate('jwt', { session: false }),
+        userController.findAll.bind(userController)
+      )
       .post(
         passport.authenticate('jwt', { session: false }),
         UserMiddleware.checkUnique,
-        UserController.create
+        userController.create.bind(userController)
       );
 
     this.router
       .route('/:id')
-      .get(passport.authenticate('jwt', { session: false }), UserController.findOne)
-      .patch(passport.authenticate('jwt', { session: false }), UserController.edit)
-      .delete(passport.authenticate('jwt', { session: false }), UserController.destroy);
+      .get(
+        passport.authenticate('jwt', { session: false }),
+        userController.findOne.bind(userController)
+      )
+      .patch(
+        passport.authenticate('jwt', { session: false }),
+        userController.edit.bind(userController)
+      )
+      .delete(
+        passport.authenticate('jwt', { session: false }),
+        userController.destroy.bind(userController)
+      );
   }
 }
 
